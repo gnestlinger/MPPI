52bc926d8627322490fc41307a423de1dcc48e35
Georg <84226458+gnestlinger@users.noreply.github.com>
2025-02-18 19:05:27 +0100

    Runtime optimizations. - Make use of vectorized operations where possible. - Proper use of graphic handles.

--- MPPIController.m
+++ MPPIController.m
@@ -1,6 +1,34 @@
+classdef MPPIController < matlab.System ...
+        & matlab.system.mixin.Propagates ...
+        & matlab.system.mixin.CustomIcon
+% MPPIController    Model Predictive Path Integral controller.
+%
+%   MPPIController properties (visualization):
+%   Visualize - Enable/disable visualization.
+%   TraceRobotPath - Trace the evolution of the robot path.
+%   FigureNbr - Print visualization to specific figure.
+% 
+%   MPPIController properties:
+%   n_samples - Number of rollout trajectories.
+%   horizon - Number of prediction steps.
+%   lambda - Temperature.
+%   nu - Exploration variance.
+%   R - Control weighting matrix.
+%   cov - Variance of control inputs disturbance.
+%   dt - Time step of controller and simulation.
+%   max_vel - Max. veloctiy.
+%   max_steer - Max. steering angle.
+% 
+%   MPPIController methods:
+%   step - 
+% 
+%   See also matlab.System.
+% 
+
 % MIT License
 % 
-% Copyright (c) 2023 Roman AdÃ¡mek
+% Copyright (c) 2023 Roman Adámek
+% Copyright (c) 2025 Georg Nestlinger
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
@@ -20,205 +48,303 @@
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 % SOFTWARE.
 
-classdef MPPIController < handle   
-    properties
-        lambda          % Temperature
-        horizon
-        n_samples
-        cov        
-        R               % Control weighting matrix
-        nu              % Exploration variance
-
-        model
-        dt
-
-        obstacles
-
-        control_sequence
-        optimized_control_sequence
-
-        goal
-        state
-        n_states = 5;
-
-        rollouts_states
-        rollouts_costs
-        rollouts_plot_handle = [];
-
-        max_vel = 5;
-        max_steer = 0.5; 
+    
+    properties (Nontunable, Logical)
+        % Visualize - Enable/disable visualization
+        %   When used in Simulink in a MATLAB System block, "Simulatie
+        %   using" must be set to "Interprete execution".
+        Visualize = true
+        
+        % TraceRobotPath - Trace the robot path
+        TraceRobotPath = true
     end
     
-    methods
-        function self = MPPIController(lambda, cov, nu, R, horizon, n_samples, model, dt, goal, obstacles)
-            self.lambda = lambda;
-            self.horizon = horizon;
-            self.n_samples = n_samples;
-            self.cov = cov;
-            self.nu = nu;
-            self.R = R;
-            self.model = model;
-            self.dt = dt;
-            self.goal = goal;
-
-            self.obstacles = obstacles;
-
-            self.control_sequence = zeros(2,self.horizon);            
-        end
+    properties (Nontunable, PositiveInteger) % Public, non-tunable properties
+        % FigureNbr - Print visualization to specific figure.
+        FigureNbr = 99
         
-        function action = get_action(self, state)
+        % n_samples - Number of rollout trajectories
+        n_samples = 400
+        
+        % horizon - Prediction horizon represented as number of steps
+        horizon = 25
+    end
+    
+    properties % Public, tunable properties
+        % lambda - Temperature selectiveness of trajectories by their costs 
+        %   The closer this value to 0, the "more" we take in considiration
+        %   controls with less cost, 0 mean use control with best cost,
+        %   huge value will lead to just taking mean of all trajectories
+        %   without cost consideration.
+        lambda = 10
+        
+        % nu - Exploration variance
+        nu = 500
+        
+        % R - Control weighting matrix
+        R = diag([1 5])
+        
+        % cov - Variance of control inputs disturbance
+        cov = [1 0.4]
+        
+        % dt - Time step of controller and simulation
+        dt = 0.1
+        
+        max_vel = 5;
+        max_steer = 0.5;
+    end
+
+    properties (SetAccess = immutable)
+        model% = VehicleModel()
+    end
+
+    properties (Access = private) % Pre-computed constants
+        control_sequence
+        optimized_control_sequence
+        
+        % Graphic handles
+        AxesHandle = gobjects(1)
+        PlotHandleTrajsRollout = gobjects(0)
+        PlotHandleTrajOpt = gobjects(1)
+        PlotHandleRobotTrace = gobjects(1)
+    end
+    
+    properties (Access = private, Nontunable)
+        NbrStates = 5
+    end
+
+    
+    
+    methods
+        function self = MPPIController(varargin)
+        % Support name-value pair arguments when constructing object
+            setProperties(self, nargin-1, varargin{2:end})
+            if nargin > 0
+                self.model = varargin{1};
+            end
+        end%Constructor
+    end
+
+    methods(Access = protected)
+        %% Common functions
+        function setupImpl(self, ~, ~, obstacles)
+        % Perform one-time calculations, such as computing constants
+            
+            self.control_sequence = zeros(numel(self.cov), self.horizon);
+            
+            if self.Visualize
+                self.setupPlot(obstacles);
+            end
+        end%fcn
+
+        function y = stepImpl(self, state, goal, obstacles)
+        % Implement algorithm. Calculate y as a function of input u and
+        % discrete states.
+        
+            [y,rollouts_states,rollouts_costs] = self.get_action(state, goal, obstacles);
+            
+            xp = coder.nullcopy(zeros(self.NbrStates, self.horizon+1));
+            xp(:,1) = state(:);
+            u = self.optimized_control_sequence;
+            mdl = self.model;
+            for i = 1:self.horizon
+                % Single trajectory step
+                xp(:,i+1) = mdl.step(u(:,i), self.dt, xp(:,i));
+            end
+
+            if self.Visualize
+                self.stepPlot(state, rollouts_states, rollouts_costs, xp);
+            end
+        end%fcn
+
+        function resetImpl(self)
+        % Initialize / reset discrete-state properties
+        end
+    end
+
+    methods (Access = private)
+        function setupPlot(self, obstacles)
+        %SETUPPLOT  Setup axes and plot handles.
+        
+            xMax = ceil(max(obstacles(:,1))/10)*10;
+            yMax = ceil(max(obstacles(:,2))/10)*10;
+            
+            fig = figure(self.FigureNbr);
+            axh = gca(fig);
+            cla(axh, 'reset')
+            self.AxesHandle = axh;
+            set(axh, 'Box','on', ...
+                'NextPlot','add', ...
+                'DataAspectRatio',[1 1 1], ...
+                'XGrid','on', ...
+                'YGrid','on', ...
+                'XLim',[-1 xMax], ...
+                'YLim',[-1 yMax]);
+            
+            % Plot obstacles
+            for i = 1:size(obstacles,1)
+                r = obstacles(i,3);
+                pos = [obstacles(i,[1 2]) - r, 2*r, 2*r];
+                rectangle(axh, 'Position',pos, 'Curvature',[1 1], ...
+                    'FaceColor', 'k', 'Edgecolor','none');
+            end
+            
+            % Initialize graphic handles for rollout trajectories
+            nans = nan(self.horizon + 1, self.n_samples);
+            legend(axh, 'AutoUpdate','off') % No legend for rollout trajectories
+            self.PlotHandleTrajsRollout = plot(axh, nans, nans, 'k-', ...
+                'LineWidth',0.5);
+            legend(axh, 'AutoUpdate','on')
+            
+            % Initialize graphic handle for robot trace/prediction
+            self.PlotHandleTrajOpt = plot(axh, nans(:,1), nans(:,1), 'g--', ...
+                'LineWidth',1, 'DisplayName','Optimal trajectory');
+            self.PlotHandleRobotTrace = plot(axh, nan, nan, 'g.-', ...
+                'MarkerSize',10, 'DisplayName','Robot trajectory');
+            set(self.PlotHandleRobotTrace, 'XData',[], 'YData',[])
+        end%fcn
+        
+        function stepPlot(self, state, rollouts_states, rollouts_costs, xp)
+        %STEPPLOT   Plot current iteration.
+        
+            minRC = min(rollouts_costs);
+            costs = (rollouts_costs - minRC)/(max(rollouts_costs) - minRC);
+%             [~,minIdx] = min(costs);
+            one = ones(self.n_samples, 1);
+            set(self.PlotHandleTrajsRollout, ...
+                {'XData'},num2cell(rollouts_states(:,:,1), 2), ...
+                {'YData'},num2cell(rollouts_states(:,:,2), 2), ...
+                {'Color'},num2cell([1 - costs 0*one 0.2*one], 2));
+            set(self.PlotHandleTrajOpt, ...(
+                'XData',xp(1,:), 'YData',xp(2,:))
+            
+            if self.TraceRobotPath
+                h = self.PlotHandleRobotTrace;
+                x = get(h, 'XData');
+                y = get(h, 'YData');
+                set(self.PlotHandleRobotTrace, 'XData', [x state(1)], 'YData',[y state(2)])
+            end
+            
+            drawnow('limitrate')
+        end%fcn
+        
+        function [action,rollouts_states,S] = get_action(self, state, goal, obstacles)
+            
             % Init variables
-            self.state = state;
-            init_state = state;
-            states = zeros(self.n_states, self.horizon+1);
+            states = coder.nullcopy(zeros(self.NbrStates, self.horizon + 1));
+            states(:,1) = state;
             S = zeros(self.n_samples, 1);
+            rollouts_states = zeros(self.n_samples, self.horizon+1, self.NbrStates);
 
-            self.rollouts_states = zeros(self.n_samples, self.horizon+1, self.n_states);
-            self.rollouts_costs = zeros(self.n_samples,1);
+            % Generate random control input disturbances            
+            delta_u = normrnd(0, self.cov, [numel(self.cov) self.n_samples self.horizon]);
+            delta_u(delta_u > 0.5) = 0.5;
+            delta_u(delta_u < -0.5) = -0.5;
 
-            % Generate random control input disturbances
-            delta_u_vel = normrnd(0, self.cov(1), [self.n_samples, self.horizon]);
-            delta_u_steer = normrnd(0, self.cov(2), [self.n_samples, self.horizon]);
-
-            delta_u_steer(delta_u_steer > 0.5) = 0.5;
-            delta_u_steer(delta_u_steer < -0.5) = -0.5;
-
-            delta_u(1,:,:) = delta_u_vel;
-            delta_u(2,:,:) = delta_u_steer;           
-
+            mdl = self.model;
+            dt = self.dt; %#ok<*PROPLC>
+            U = self.control_sequence;
             for k = 1:self.n_samples
-                states(:,1) = init_state;
-                for i = 1:self.horizon                    
+                duk = permute(delta_u(:,k,:), [1 3 2]);
+                
+                u = U + duk;
+                for i = 1:self.horizon
                     % Single trajectory step
-                    states(:,i+1) = self.model.step(self.control_sequence(:,i) + delta_u(:, k, i), self.dt, states(:,i));
+                    tmp = mdl.step(u(:,i), dt, states(:,i));
+                    states(:,i+1) = tmp;
+                end
+                
+                % Compute cost of the state
+                S(k) = sum(self.cost_function(states(:,2:end), U, duk, goal, obstacles));
 
-                    % Compute cost of the state
-                    S(k) = S(k) + self.cost_function(states(:,i+1), self.control_sequence(:,i), delta_u(:, k, i));
-%                     fprintf("Cost: %f\n", self.cost_function(states(:,i+1)));
-
-                end 
-%                 fprintf("Actions for %d rollout: \n", k);
-%                 self.control_sequence() + reshape(delta_u(:, k, :), 2,self.horizon)
-
-%                 fprintf("States for %d trajectory: \n", k);
-
-%                 fprintf("Costs for %d trajectory: \n", k);
-                self.rollouts_states(k,:,:) = states';
-                self.rollouts_costs(k) = S(k,end);
-            end     
-
+                rollouts_states(k,:,:) = states';
+            end%for
+            
             % Update the control input according to the expectation over K sample trajectories
             S_normalized = S - min(S);
-            for i = 1:self.horizon
-                self.control_sequence(:,i) = self.control_sequence(:,i) + self.total_entropy(delta_u(:,:,i)', S_normalized(:))'; 
-            end
+            entropy = self.total_entropy(delta_u, S_normalized);
+            U = U + permute(entropy, [2 3 1]); 
+            
             % Output saturation
-            self.control_sequence(1, self.control_sequence(1,:) > self.max_vel) = self.max_vel;
-            self.control_sequence(1, self.control_sequence(1,:) < -self.max_vel) = -self.max_vel;
-
-            self.control_sequence(2, self.control_sequence(2,:) > self.max_steer) = self.max_steer;
-            self.control_sequence(2, self.control_sequence(2,:) < -self.max_steer) = -self.max_steer;
+            vMax = self.max_vel;
+            sMax = self.max_steer;
+            U(1, U(1,:) > +vMax) = +vMax;
+            U(1, U(1,:) < -vMax) = -vMax;
+            U(2, U(2,:) > +sMax) = +sMax;
+            U(2, U(2,:) < -sMax) = -sMax;
+            self.control_sequence = U;
 
             % Select control action
-            self.optimized_control_sequence = self.control_sequence;
-
-            action = self.control_sequence(:,1);
+            self.optimized_control_sequence = U;
+            
+            action = U(:,1);
             self.control_sequence = [self.control_sequence(:,2:end), [0; 0]];
-        end
-
-        function cost = cost_function(self, state, u, du)
-           state_cost = self.state_cost_function(state);
+        end%fcn
+        
+        function cost = cost_function(self, state, u, du, goal, obstacles)
+           state_cost = self.state_cost_function(state, goal, obstacles);
            control_cost = self.control_cost_function(u, du);
-
+           
            cost = state_cost + control_cost;
         end
 
-        function cost = state_cost_function(self, state)
-            obstacle_cost = self.obstacle_cost_function(state);
-            heading_cost = self.heading_cost_function(state);
-            distance_cost = self.distance_cost_function(state);%norm(self.goal(1:2) - state(1:2))^2*5;
+        function cost = state_cost_function(self, state, goal, obstacles)
+            obstacle_cost = self.obstacle_cost_function(state(1,:), state(2,:), obstacles);
+            heading_cost = self.heading_cost_function(state(3,:), goal(3));
+            distance_cost = self.distance_cost_function(state(1,:), state(2,:), goal);%norm(self.goal(1:2) - state(1:2))^2*5;
 
-            cost = distance_cost + heading_cost + obstacle_cost;     
+            cost = distance_cost + heading_cost + obstacle_cost;
         end
 
-        function cost = distance_cost_function(self, state)
+        function cost = distance_cost_function(self, x, y, goal)
             weight = 100;
 %             cost = 100*norm(self.goal(1:2) - state(1:2))^2;
-            cost = weight*(self.goal(1:2) - state(1:2)')*(self.goal(1:2) - state(1:2)')';
-        end
+            cost = weight*hypot(goal(1) - x, goal(2) - y).^2;
+        end%fcn
 
-        function cost = heading_cost_function(self, state)
+        function cost = heading_cost_function(self, yaw, yawGoal)
             weight = 50;
             pow = 2;
-            cost = weight*abs(self.get_angle_diff(self.goal(3),state(3)))^pow;
-        end
+            cost = weight*abs(get_angle_diff(yawGoal, yaw)).^pow;
+        end%fcn
         
         function cost = control_cost_function(self, u, du)
-            cost = (1-1/self.nu)/2 * du'*self.R*du + u'*self.R*du + 1/2*u'*self.R*u;
+%             cost = (1-1/self.nu)/2 * du'*self.R*du + u'*self.R*du + 1/2*u'*self.R*u;
+            
+            RR = self.R;
+            cost = 0.5*(1-1/self.nu)*sum((du'*RR.*du'), 2)' ...
+                + sum((u'*RR.*du'), 2)' ...
+                +0.5*sum((u'*RR.*u'), 2)';
         end
-
-        function [obstacle_cost] = obstacle_cost_function(self, state)
-            if isempty(self.obstacles)
+        
+        function obstacle_cost = obstacle_cost_function(self, x, y, obstacles)
+                        
+            if isempty(obstacles)
                 obstacle_cost = 0;
                 return
             end
-            distance_to_obstacle = sqrt(sum((state(1:2)' - self.obstacles(:,[1,2])).^2,2));
-            [min_dist, min_dist_idx] = min(distance_to_obstacle);
+            distance_to_obstacle = hypot(obstacles(:,1) - x, obstacles(:,2) - y);
+            [min_dist,min_dist_idx] = min(distance_to_obstacle, [], 1);
             
-            if min_dist <= self.obstacles(min_dist_idx,3)
-                hit = 1;
-            else
-                hit = 0;
-            end
-
+            hit = double(min_dist < obstacles(min_dist_idx,3)');
             obstacle_cost = 550*exp(-min_dist/5) + 1e6*hit;
         end
-
+        
         function value = total_entropy(self, du, trajectory_cost)
             exponents = exp(-1/self.lambda * trajectory_cost);
-
-            value = sum(exponents.*du ./ sum(exponents),1);
-        end
-
-        function plot_rollouts(self, fig)
-            if ~isempty(self.rollouts_plot_handle)
-                for i = 1:length(self.rollouts_plot_handle)
-                    delete(self.rollouts_plot_handle(i));
-                end
-                self.rollouts_plot_handle = [];
-            end
-            figure(fig)
-            costs = (self.rollouts_costs - min(self.rollouts_costs))/(max(self.rollouts_costs) - min(self.rollouts_costs));
-            [~, min_idx] = min(costs);
-            for i = 1:self.n_samples
-                if i == min_idx
-                    color = [0, 1, 1];
-                else
-                    color = [1-costs(i), 0, 0.2];
-                end
-                self.rollouts_plot_handle(end+1) = plot(self.rollouts_states(i,:,1), self.rollouts_states(i,:,2),'-', 'Color', color);
-%                 self.rollouts_plot_handle(end+1) = text(self.rollouts_states(i,end,1), self.rollouts_states(i,end,2), string(self.rollouts_costs(i)));
-            end
-
-            % Rollout of selected trajectory
-            states = zeros(self.n_states, self.horizon+1);
-            states(:,1) = self.state;
-
-            for i = 1:self.horizon                    
-                % Single trajectory step
-                states(:,i+1) = self.model.step(self.optimized_control_sequence(:,i), self.dt, states(:,i));
-            end
-            self.rollouts_plot_handle(end+1) = plot(states(1,:), states(2,:), '--', 'Color', [0,1,0]);
-
-        end     
-    end
-
-    methods(Static)
-        function angle = get_angle_diff(angle1, angle2)
-            angle_diff = angle1-angle2;
-            angle = mod(angle_diff+pi, 2*pi) - pi;              
+            
+            value = sum(exponents.*permute(du, [2 1 3]) ./ sum(exponents), 1);
         end
     end
+        
+end%class
 
+
+function angle = get_angle_diff(angle1, angle2)
+angle_diff = angle1 - angle2;
+angle = mod(angle_diff + pi, 2*pi) - pi;
 end
 
+function r = normrnd(mu, sigma, sz)
+r = randn(sz).*sigma(:) + mu;
+end%fcn
--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 % MIT License
 % 
-% Copyright (c) 2023 Roman Ad√°mek
+% Copyright (c) 2023 Roman Ad·mek
+% Copyright (c) 2025 Georg Nestlinger
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
@@ -20,80 +21,33 @@
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 % SOFTWARE.
 
-clc, clear, close all
+clc, clear
+rng(1)
 
 %% Param defintion
-n_samples = 400;    % Number of rollout trajectories
-horizon = 25;       % Prediction horizon represented as number of steps
-lambda = 10;        % Temperature - Selectiveness of trajectories by their costs (The closer this value to 0, the "more" we take in considiration controls with less cost), 0 mean use control with best cost, huge value will lead to just taking mean of all trajectories without cost consideration
-nu = 500;           % Exploration variance
-R = diag([1, 5]);   % Control weighting matrix
-cov = [1, 0.4];     % Variance of control inputs disturbance 
-dt = 0.1;           % Time step of controller and simulation
-
 init_state = [0, 0, 0, 0, 0]; % x, y, phi, v, steer
 goal_state = [6, 6, 0];
 
-%% Define environment - obstacles [x, y, radius]
-obstacles = [];
-% obstacles = [1.5,1, 0.5;
-%     3 4 0.5;
-%     1 3 0.5;
-%     4 3 0.5;
-%     2.5 2.5 0.5
-%     ]; % x, y, radius
-% obstacles = [2,0,0.5];
 
+%% Define environment - obstacles [x, y, radius]
 n_obstacles = 40;
-obstacles = [rand(n_obstacles,2)*4+1, 0.2*ones(n_obstacles,1)];
+obstacles = [rand(n_obstacles, 2)*4+1, 0.2*ones(n_obstacles, 1)];
+
 
 %% Init
 car_real = VehicleModel();
 car = VehicleModel();
-controller = MPPIController(lambda, cov, nu, R, horizon, n_samples, car, dt, goal_state, obstacles);
+controller = MPPIController(car);
+controller.Visualize = false;
 
-%% Prepare visualisation
-fig = figure;
-
-hold on
-axis equal
-xlim([-0.5 + min(init_state(1), goal_state(1)), 0.5 + max(init_state(1), goal_state(1))]);
-ylim([-0.5 + min(init_state(2), goal_state(2)), 0.5 + max(init_state(2), goal_state(2))]);
-plot_state(init_state, 'bo');
-plot_state(goal_state, 'ro');
-plot_obstacles(obstacles);
-
-%% Control
 car_state = init_state;
-
-for i = 1:100    
-    action = controller.get_action(car_state);
-    controller.plot_rollouts(fig);
+dt = controller.dt;
+for i = 1:100
+    action = controller.step(car_state, goal_state, obstacles);
     
     car_state = car_real.step(action, dt, car_state);
-    plot_state(car_state, 'go')
-
-    exportgraphics(gcf,'animation.gif','Append',true);
-    pause % Step the control loop by pressing any key    
+    
+    % Exporting to animated GIF requires at least R2022a
+    %exportgraphics(gcf,'animation.gif','Append',true);
 end
 
-%% Utility functions
-
-function plot_obstacles(obstacles) 
-    for i = 1:size(obstacles,1)
-        r = obstacles(i,3);
-        pos = [obstacles(i,[1,2])-r 2*r 2*r];
-        rectangle('Position',pos,'Curvature',[1 1], 'FaceColor', 'k', 'Edgecolor','none');
-    end
-end
-
-
-function plot_state(state, style)
-    x = state(1);
-    y = state(2);
-    phi = state(3);
-
-    plot(x, y, style);
-    [delta_x, delta_y] = pol2cart(phi, 0.5);
-    quiver(x, y, delta_x, delta_y)
-end
