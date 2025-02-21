7060561fad87abc59aaf64a2a416dfc9cd4503ed
Georg <84226458+gnestlinger@users.noreply.github.com>
2025-02-21 22:18:25 +0100

    Code cleanup.

--- MPPIController.m
+++ MPPIController.m
@@ -9,51 +9,51 @@ classdef MPPIController < matlab.System ...
 %   FigureNbr - Print visualization to specific figure.
 % 
 %   MPPIController properties:
-%   n_samples - Number of rollout trajectories.
-%   horizon - Number of prediction steps.
-%   lambda - Temperature.
+%   NbrTrajs - Number of rollout trajectories.
+%   NbrPredSteps - Number of prediction steps.
+%   Lambda - Temperature.
 %   nu - Exploration variance.
 %   R - Control weighting matrix.
 %   cov - Variance of control inputs disturbance.
-%   dt - Time step of controller and simulation.
+%   TimeStep - Prediction time step.
 %   max_vel - Max. veloctiy.
 %   max_steer - Max. steering angle.
 % 
 %   MPPIController methods:
-%   step - 
+%   step - Compute control action for current state.
 % 
 %   See also matlab.System.
-% 
 
 % MIT License
 % 
 % Copyright (c) 2023 Roman Adámek
 % Copyright (c) 2025 Georg Nestlinger
 % 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-% copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions:
+% Permission is hereby granted, free of charge, to any person obtaining a
+% copy of this software and associated documentation files (the
+% "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to permit
+% persons to whom the Software is furnished to do so, subject to the
+% following conditions:
 % 
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
+% The above copyright notice and this permission notice shall be included
+% in all copies or substantial portions of the Software.
 % 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-% SOFTWARE.
-
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+% NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+% USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
     
     properties (Nontunable, Logical)
         % Visualize - Enable/disable visualization
-        %   When used in Simulink in a MATLAB System block, "Simulatie
-        %   using" must be set to "Interprete execution".
-        Visualize = true
+        %   When used in Simulink in a MATLAB System block, "Simulate
+        %   using" must be set to "Interpreted execution".
+        Visualize = false
         
         % TraceRobotPath - Trace the robot path
         TraceRobotPath = true
@@ -63,20 +63,20 @@ classdef MPPIController < matlab.System ...
         % FigureNbr - Print visualization to specific figure.
         FigureNbr = 99
         
-        % n_samples - Number of rollout trajectories
-        n_samples = 400
+        % NbrTrajs - Number of rollout trajectories
+        NbrTrajs = 400
         
-        % horizon - Prediction horizon represented as number of steps
-        horizon = 25
+        % NbrPredSteps - Number of prediction steps
+        NbrPredSteps = 25
     end
     
     properties % Public, tunable properties
-        % lambda - Temperature selectiveness of trajectories by their costs 
+        % Lambda - Temperature selectiveness of trajectories by their costs 
         %   The closer this value to 0, the "more" we take in considiration
         %   controls with less cost, 0 mean use control with best cost,
         %   huge value will lead to just taking mean of all trajectories
         %   without cost consideration.
-        lambda = 10
+        Lambda = 10
         
         % nu - Exploration variance
         nu = 500
@@ -87,32 +87,33 @@ classdef MPPIController < matlab.System ...
         % cov - Variance of control inputs disturbance
         cov = [1 0.4]
         
-        % dt - Time step of controller and simulation
-        dt = 0.1
+        % TimeStep - Prediction time step (sec)
+        TimeStep = 0.1
         
         max_vel = 5;
         max_steer = 0.5;
     end
 
     properties (SetAccess = immutable)
-        model% = VehicleModel()
+        Model% = VehicleModel()
     end
 
     properties (Access = private) % Pre-computed constants
         control_sequence
-        optimized_control_sequence
         
         % Graphic handles
         AxesHandle = gobjects(1)
         PlotHandleTrajsRollout = gobjects(0)
         PlotHandleTrajOpt = gobjects(1)
         PlotHandleRobotTrace = gobjects(1)
+        PlotHandleTitle = gobjects(1)
+        NSTepCalls = 0
     end
     
     properties (Access = private, Nontunable)
         NbrStates = 5
     end
-
+    
     
     
     methods
@@ -120,48 +121,94 @@ classdef MPPIController < matlab.System ...
         % Support name-value pair arguments when constructing object
             setProperties(self, nargin-1, varargin{2:end})
             if nargin > 0
-                self.model = varargin{1};
+                self.Model = varargin{1};
             end
         end%Constructor
     end
-
+    
     methods(Access = protected)
         %% Common functions
         function setupImpl(self, ~, ~, obstacles)
         % Perform one-time calculations, such as computing constants
             
-            self.control_sequence = zeros(numel(self.cov), self.horizon);
+            self.control_sequence = zeros(numel(self.cov), self.NbrPredSteps);
             
             if self.Visualize
                 self.setupPlot(obstacles);
             end
         end%fcn
-
+        
         function y = stepImpl(self, state, goal, obstacles)
         % Implement algorithm. Calculate y as a function of input u and
         % discrete states.
-        
-            [y,rollouts_states,rollouts_costs] = self.get_action(state, goal, obstacles);
             
-            xp = coder.nullcopy(zeros(self.NbrStates, self.horizon+1));
+            % Init variables
+            states = coder.nullcopy(zeros(self.NbrStates, self.NbrPredSteps + 1));
+            states(:,1) = state;
+            S = zeros(self.NbrTrajs, 1);
+            rollouts_states = zeros(self.NbrTrajs, self.NbrPredSteps + 1, self.NbrStates);
+            
+            % Generate random control input disturbances
+            du = normrnd(0, self.cov, [numel(self.cov) self.NbrTrajs self.NbrPredSteps]);
+            du(du > 0.5) = 0.5;
+            du(du < -0.5) = -0.5;
+            
+            mdl = self.Model;
+            dt = self.TimeStep;
+            self.control_sequence(:,1) = 0;
+            U = circshift(self.control_sequence, -1, 2);
+            for k = 1:self.NbrTrajs
+                duk = permute(du(:,k,:), [1 3 2]);
+                
+                u = U + duk;
+                for i = 1:self.NbrPredSteps
+                    % Single trajectory step
+                    tmp = mdl.step(u(:,i), dt, states(:,i));
+                    states(:,i+1) = tmp;
+                end
+                
+                % Compute cost of the state
+                S(k) = sum(self.cost_function(states(:,2:end), U, duk, goal, obstacles));
+                
+                rollouts_states(k,:,:) = states';
+            end%for
+            
+            % Update the control input according to the expectation over K
+            % sample trajectories
+            entropy = self.total_entropy(du, S - min(S));
+            U = U + permute(entropy, [2 3 1]); 
+            
+            % Output saturation
+            vMax = self.max_vel;
+            sMax = self.max_steer;
+            U(1, U(1,:) > +vMax) = +vMax;
+            U(1, U(1,:) < -vMax) = -vMax;
+            U(2, U(2,:) > +sMax) = +sMax;
+            U(2, U(2,:) < -sMax) = -sMax;
+            self.control_sequence = U;
+            
+            % Select control action
+            y = U(:,1);
+            
+            % Predicted states w.r.t. optimal control sequence
+            xp = coder.nullcopy(zeros(self.NbrStates, self.NbrPredSteps + 1));
             xp(:,1) = state(:);
-            u = self.optimized_control_sequence;
-            mdl = self.model;
-            for i = 1:self.horizon
+            for i = 1:self.NbrPredSteps
                 % Single trajectory step
-                xp(:,i+1) = mdl.step(u(:,i), self.dt, xp(:,i));
+                xp(:,i+1) = mdl.step(U(:,i), self.TimeStep, xp(:,i));
             end
-
+            
             if self.Visualize
-                self.stepPlot(state, rollouts_states, rollouts_costs, xp);
+                self.NSTepCalls = self.NSTepCalls + 1;
+                self.stepPlot(state, rollouts_states, S, xp);
             end
         end%fcn
-
+        
         function resetImpl(self)
         % Initialize / reset discrete-state properties
         end
     end
-
+    
     methods (Access = private)
         function setupPlot(self, obstacles)
         %SETUPPLOT  Setup axes and plot handles.
@@ -190,7 +237,7 @@ classdef MPPIController < matlab.System ...
             end
             
             % Initialize graphic handles for rollout trajectories
-            nans = nan(self.horizon + 1, self.n_samples);
+            nans = nan(self.NbrPredSteps + 1, self.NbrTrajs);
             legend(axh, 'AutoUpdate','off') % No legend for rollout trajectories
             self.PlotHandleTrajsRollout = plot(axh, nans, nans, 'k-', ...
                 'LineWidth',0.5);
@@ -202,83 +249,34 @@ classdef MPPIController < matlab.System ...
             self.PlotHandleRobotTrace = plot(axh, nan, nan, 'g.-', ...
                 'MarkerSize',10, 'DisplayName','Robot trajectory');
             set(self.PlotHandleRobotTrace, 'XData',[], 'YData',[])
+            
+            % Title
+            self.PlotHandleTitle = title(axh, sprintf('t = %.2f', 0));
         end%fcn
         
         function stepPlot(self, state, rollouts_states, rollouts_costs, xp)
         %STEPPLOT   Plot current iteration.
         
+            t = self.NSTepCalls*self.TimeStep;
+            set(self.PlotHandleTitle, 'String',sprintf('t = %.2f sec', t));
+            
             minRC = min(rollouts_costs);
             costs = (rollouts_costs - minRC)/(max(rollouts_costs) - minRC);
-%             [~,minIdx] = min(costs);
-            one = ones(self.n_samples, 1);
+            one = ones(self.NbrTrajs, 1);
             set(self.PlotHandleTrajsRollout, ...
                 {'XData'},num2cell(rollouts_states(:,:,1), 2), ...
                 {'YData'},num2cell(rollouts_states(:,:,2), 2), ...
                 {'Color'},num2cell([1 - costs 0*one 0.2*one], 2));
-            set(self.PlotHandleTrajOpt, ...(
-                'XData',xp(1,:), 'YData',xp(2,:))
+            set(self.PlotHandleTrajOpt, 'XData',xp(1,:), 'YData',xp(2,:))
             
             if self.TraceRobotPath
                 h = self.PlotHandleRobotTrace;
                 x = get(h, 'XData');
                 y = get(h, 'YData');
-                set(self.PlotHandleRobotTrace, 'XData', [x state(1)], 'YData',[y state(2)])
+                set(h, 'XData', [x state(1)], 'YData',[y state(2)])
             end
             
             drawnow('limitrate')
-        end%fcn
-        
-        function [action,rollouts_states,S] = get_action(self, state, goal, obstacles)
-            
-            % Init variables
-            states = coder.nullcopy(zeros(self.NbrStates, self.horizon + 1));
-            states(:,1) = state;
-            S = zeros(self.n_samples, 1);
-            rollouts_states = zeros(self.n_samples, self.horizon+1, self.NbrStates);
-
-            % Generate random control input disturbances            
-            delta_u = normrnd(0, self.cov, [numel(self.cov) self.n_samples self.horizon]);
-            delta_u(delta_u > 0.5) = 0.5;
-            delta_u(delta_u < -0.5) = -0.5;
-
-            mdl = self.model;
-            dt = self.dt; %#ok<*PROPLC>
-            U = self.control_sequence;
-            for k = 1:self.n_samples
-                duk = permute(delta_u(:,k,:), [1 3 2]);
-                
-                u = U + duk;
-                for i = 1:self.horizon
-                    % Single trajectory step
-                    tmp = mdl.step(u(:,i), dt, states(:,i));
-                    states(:,i+1) = tmp;
-                end
-                
-                % Compute cost of the state
-                S(k) = sum(self.cost_function(states(:,2:end), U, duk, goal, obstacles));
-
-                rollouts_states(k,:,:) = states';
-            end%for
-            
-            % Update the control input according to the expectation over K sample trajectories
-            S_normalized = S - min(S);
-            entropy = self.total_entropy(delta_u, S_normalized);
-            U = U + permute(entropy, [2 3 1]); 
-            
-            % Output saturation
-            vMax = self.max_vel;
-            sMax = self.max_steer;
-            U(1, U(1,:) > +vMax) = +vMax;
-            U(1, U(1,:) < -vMax) = -vMax;
-            U(2, U(2,:) > +sMax) = +sMax;
-            U(2, U(2,:) < -sMax) = -sMax;
-            self.control_sequence = U;
-
-            % Select control action
-            self.optimized_control_sequence = U;
-            
-            action = U(:,1);
-            self.control_sequence = [self.control_sequence(:,2:end), [0; 0]];
         end%fcn
         
         function cost = cost_function(self, state, u, du, goal, obstacles)
@@ -331,19 +329,18 @@ classdef MPPIController < matlab.System ...
         end
         
         function value = total_entropy(self, du, trajectory_cost)
-            exponents = exp(-1/self.lambda * trajectory_cost);
+            exponents = exp(-1/self.Lambda * trajectory_cost);
             
             value = sum(exponents.*permute(du, [2 1 3]) ./ sum(exponents), 1);
         end
     end
-        
+    
 end%class
 
 
 function angle = get_angle_diff(angle1, angle2)
-angle_diff = angle1 - angle2;
-angle = mod(angle_diff + pi, 2*pi) - pi;
-end
+angle = mod(angle1 - angle2 + pi, 2*pi) - pi;
+end%fcn
 
 function r = normrnd(mu, sigma, sz)
 r = randn(sz).*sigma(:) + mu;
--- main.m
+++ main.m
@@ -3,23 +3,24 @@
 % Copyright (c) 2023 Roman Adámek
 % Copyright (c) 2025 Georg Nestlinger
 % 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-% copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions:
+% Permission is hereby granted, free of charge, to any person obtaining a
+% copy of this software and associated documentation files (the
+% "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to permit
+% persons to whom the Software is furnished to do so, subject to the
+% following conditions:
 % 
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
+% The above copyright notice and this permission notice shall be included
+% in all copies or substantial portions of the Software.
 % 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-% SOFTWARE.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+% NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+% USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 clc, clear
 rng(1)
@@ -38,10 +39,10 @@ obstacles = [rand(n_obstacles, 2)*4+1, 0.2*ones(n_obstacles, 1)];
 car_real = VehicleModel();
 car = VehicleModel();
 controller = MPPIController(car);
-controller.Visualize = false;
+controller.Visualize = true;
 
 car_state = init_state;
-dt = controller.dt;
+dt = controller.TimeStep;
 for i = 1:100
     action = controller.step(car_state, goal_state, obstacles);
     
